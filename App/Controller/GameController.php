27c1fca0756e058c19de09d9f0d9cbcf1fd8a18c
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-11-25 18:19:57 -0300

    feature - criando os métodos no controller

--- App/Controller/GameController.php
+++ App/Controller/GameController.php
@@ -1,11 +1,29 @@
 <?php
 
 namespace App\Controller;
+use App\Entity\Game;
 
 Class GameController{
 
-    public function __construct()
-    {
-        echo 'chamou a game controller';
+    public function create($data = null){
+        return json_encode(["name" => "create"]);
     }
+
+    public function update($id = 0, $data = null){
+        return json_encode(["name" => "create"]);
+    }
+
+    public function delete($id = 0){
+        return json_encode(["name" => "create"]);
+    }
+
+    public function readById($id = 0){
+        return json_encode(["name" => "create"]);
+    }
+
+    public function readAll(){
+        return json_encode(["name" => "create"]);
+    }
+
+
 }
